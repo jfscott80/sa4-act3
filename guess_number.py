18e5ead87310c7ff5319f3c30aaf28e713410287
John Scott <jfscott@memphis.edu>
2024-02-03 20:23:57 -0600

    Resolved conflicts correctly

--- guess_number.py
+++ guess_number.py
@@ -1,19 +1,15 @@
 number = 10
 
 print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of?" ))
+guess = int(input("What number am I thinking of? " ))
 i = 4
 while guess != number:
     i -= 1
     if guess == 'q' or i == 0:
         print(f"Sorry! The number was {number}.")
         break
-    guess = input(f"That's not the number. You have {i} guesses left. Guess again or press (q) to quit: ")
-guess = int(input("What number am I thinking of? "))
-
-while guess != number:
     try:
-        guess = int(input("That's not the number. Guess again or press (q) to quit: "))
+        guess = int(input(f"That's not the number. You have {i} guesses left. Guess again or press (q) to quit: "))
     except:
         print(f"Sorry! The number was {number}.")
         break
