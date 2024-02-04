a376df1af3ee9a049e41ed64f4ae17bc98b2d8b5
John Scott <jfscott@memphis.edu>
2024-02-03 20:16:39 -0600

    decrement to establish limited guesses

--- guess_number.py
+++ guess_number.py
@@ -2,12 +2,14 @@ number = 10
 
 print("I'm thinking of a number...")
 guess = int(input("What number am I thinking of?" ))
-
+i = 4
 while guess != number:
-    guess = input("That's not the number. Guess again or press (q) to quit: ")
-    if guess == 'q':
+    i -= 1
+    if guess == 'q' or i == 0:
         print(f"Sorry! The number was {number}.")
         break
+    guess = input(f"That's not the number. You have {i} guesses left. Guess again or press (q) to quit: ")
+
 if guess == number:
     print("Congratulations! You guessed the right number.")
 
