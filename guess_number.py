857ee42a3495a1217e87efae43fe1531079a1ef3
John Scott <jfscott@memphis.edu>
2024-02-03 19:42:11 -0600

    Set loop to allow multiple guesses by user with (q)uit option.

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,13 @@
 number = 10
 
 print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of?"))
+guess = int(input("What number am I thinking of?" ))
 
+while guess != number:
+    guess = input("That's not the number. Guess again or press (q) to quit: ")
+    if guess == 'q':
+        print(f"Sorry! The number was {number}.")
+        break
 if guess == number:
     print("Congratulations! You guessed the right number.")
-else:
-    print(f"Sorry! The number was {number}.")
+
